edb7f3c8f1f19276da53eed2cd68e3c4192c6489
Gilad Lev-Ari <giladle@cet.ac.il>
2023-04-21 18:22:33 +0300

    First commit

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common';
+import Next from 'next';
+import { RenderModule } from 'nest-next';
 
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { resolve } from 'path';
+
+console.log('GILAD', resolve(__dirname, '../../client/.next/server'));
 
 @Module({
-  imports: [],
+  imports: [
+    RenderModule.forRootAsync(
+      Next({
+        dev: process.env.NODE_ENV !== 'production',
+        dir: resolve(__dirname, '../client/.next/server'),
+      }),
+      {
+        viewsDir: '',
+      }
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
